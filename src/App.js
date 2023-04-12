f66fc8426c73799c69410c62a731e6ab1a2934a7
Eric Furrevig <efurrevig@gmail.com>
2023-04-12 18:42:51 -0400

    css stuff

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { useState, useEffect } from 'react'
 import Note from './components/Note'
 import noteService from './services/notes'
+import Notification from './components/Notification'
 
 const App = (props) => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [successMessage, setSuccessMessage] = useState(null)
 
   useEffect(() => {
     noteService
@@ -22,10 +25,20 @@ const App = (props) => {
       important: Math.random() < 0.5,
     }
 
-    noteService.create(noteObject).then(returnedNote => {
-      setNotes(notes.concat(returnedNote))
-      setNewNote('')
-    })
+    noteService
+      .create(noteObject)
+      .then(returnedNote => {
+        setNotes(notes.concat(returnedNote))
+        setNewNote('')
+      })
+      .then(() => {
+        setSuccessMessage(
+          `Note '${noteObject.content}' was added to server`
+        )
+        setTimeout(() => {
+          setSuccessMessage(null)
+        }, 3000)
+      })
   }
 
   const handleNoteChange = (event) => {
@@ -43,9 +56,12 @@ const App = (props) => {
         setNotes(notes.map(note => note.id !== id ? note : returnedNote))
       })
       .catch (error => {
-        alert(
-          `the note '${note.content}' was already deleted from the server`
+        setErrorMessage(
+          `Note '${note.content}' was already removed from server`
         )
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
         setNotes(notes.filter(n => n.id !== id))
       })
   }
@@ -57,6 +73,8 @@ const App = (props) => {
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage} messageType='error' />
+      <Notification message={successMessage} messageType='success' />
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all'}
